6e14fa4ef34d81c279b88675e6d36f941e480426
Chris Sharp <christopher.sharp@hotmail.co.uk>
2024-08-16 17:21:04 +0100

    Update to get all tracks in the list instead of the first 100

--- DownloadToMp3.py
+++ DownloadToMp3.py
@@ -8,8 +8,10 @@ import subprocess
 
 
 def convert_to_mp3(file, output):
-    # Surround file and output paths with double quotes to handle spaces
-    cmd = 'ffmpeg -i "%s"' % file + ' -vn -ar 44100 -ac 2 -ab 192k -f mp3 "%s"' % output
+    cmd = (
+        'ffmpeg -i "%s"' % file
+        + ' -vn -ar 44100 -ac 2 -ab 192k -f mp3 "%s" > /dev/null 2>&1' % output
+    )
     subprocess.call(cmd, shell=True)
 
 
@@ -26,10 +28,22 @@ def authenticate_user():
 
 def get_spotify_track_list(sp, playlist_link):
     playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+
+    print("Getting Playlist Info")
+    print("Playlist URI:", playlist_URI)
+    print("Playlist Name:", sp.playlist(playlist_URI)["name"])
+    print("Playlist Description:", sp.playlist(playlist_URI)["description"])
+
+    results = sp.playlist_tracks(playlist_URI)
+    tracks = results["items"]
+    while results["next"]:
+        results = sp.next(results)
+        tracks.extend(results["items"])
+
     track_info = [
-        [x["track"]["name"], x["track"]["artists"][0]["name"]]
-        for x in sp.playlist_tracks(playlist_URI)["items"]
+        [x["track"]["name"], x["track"]["artists"][0]["name"]] for x in tracks
     ]
+
     return track_info
 
 
@@ -38,29 +52,30 @@ def get_youtube_link(song_name):
     return videosSearch.result()["result"][0]["link"]
 
 
-def download_song(song_name, folder_name):
+def download_song(index, song_name, folder_name, total_songs):
     song_name = track[0] + " - " + track[1]
-    print("Downloading Song:", song_name[:30])
+    print("Downloading Song:", song_name, f"({index+1}/{total_songs})")
 
-    file_exists = os.path.exists(f"{folder_name}/{song_name}.wav")
+    file_exists = os.path.exists(f"Downloads/{folder_name}/{song_name}.mp3")
 
-    if not file_exists:
-        video_link = get_youtube_link(song_name)
-        video = YouTube(video_link)
-        stream = video.streams.filter(only_audio=True).first()
-        stream.download(filename=f"{folder_name}/{song_name}.wav")
-        convert_to_mp3(
-            f"{folder_name}/{song_name}.wav",
-            f"{folder_name}/{song_name}.mp3",
-        )
+    try:
+        if not file_exists:
+            video_link = get_youtube_link(song_name)
+            video = YouTube(video_link)
+            stream = video.streams.filter(only_audio=True).first()
+            stream.download(filename=f"Downloads/{folder_name}/{song_name}.wav")
+            convert_to_mp3(
+                f"Downloads/{folder_name}/{song_name}.wav",
+                f"Downloads/{folder_name}/{song_name}.mp3",
+            )
 
-        # Delete the wav file
-        os.remove(f"{folder_name}/{song_name}.wav")
-
-        print("Downloading Complete")
-
-    else:
-        print("Downloading Skipped (File Already Exists)", song_name[:30])
+            os.remove(f"Downloads/{folder_name}/{song_name}.wav")
+            print("Downloading Complete")
+        else:
+            print("Downloading Skipped - File Already Exists")
+    except Exception as e:
+        print("Downloading Skipped - Error Occurred")
+        print(e)
 
 
 if __name__ == "__main__":
@@ -69,15 +84,18 @@ if __name__ == "__main__":
     sp = authenticate_user()
     folder_name = "Tech House"
     playlist_link = (
-        "https://open.spotify.com/playlist/5ceLorHEjpOjqRbIFcUPa9?si=9d045ef1fbde4e64"
+        "https://open.spotify.com/playlist/5ceLorHEjpOjqRbIFcUPa9?si=5a1beb0b6ee542ac"
     )
     track_names = get_spotify_track_list(sp, playlist_link)
 
-    if not os.path.exists(folder_name):
-        os.mkdir(folder_name)
+    if not os.path.exists(f"Downloads/{folder_name}"):
+        os.mkdir(f"Downloads/{folder_name}")
 
-    for track in track_names:
+    for index, track in enumerate(track_names):
         try:
-            download_song(track, folder_name)
+            download_song(index, track, folder_name, len(track_names))
         except KeyError:
             print("Unable to Download Song:", track[0])
+
+    print("All Songs Downloaded")
+    print("Total Songs Downloaded:", len(track_names))
